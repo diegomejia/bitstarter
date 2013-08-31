95618d19c26d3ded195d0124aff3a2100550cfac
Diego Mejia <diegomejia87@gmail.com>
2013-08-30 22:40:34 -0700

    Added slash to index file name

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-   fs.readFile('index.html', function(err, data) {
+   fs.readFile('/index.html', function(err, data) {
       if(err) throw err;
       response.send(buf.toString(data));
    }
